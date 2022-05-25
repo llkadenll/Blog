2a2f4abb9ae60548da8b9e6134068619a5963d1f
llkadenll <kdwornik@o2.pl>
2022-05-26 01:28:36 +0200

    Validate post body length & uniqueness of title

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,6 +1,7 @@
 class Post < ApplicationRecord
   belongs_to :category, optional: true
 
-  validates :title, presence: true, uniqueness: true
-  validates :body, presence: true
+  validates :title, presence: true, uniqueness: { scope: :author_id, case_sensitive: false}
+  validates :body, length: { minimum: 10 }
+
 end
