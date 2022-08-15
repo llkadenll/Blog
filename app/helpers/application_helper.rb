e0756012890b5128670d4d26c8ba2ff262e59f1f
llkadenll <kdwornik@o2.pl>
2022-08-16 00:43:02 +0200

    Modify alerts

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,3 +1,19 @@
 module ApplicationHelper
   include Pagy::Frontend
+
+  def flash_class(flash)
+    if flash.alert
+      'alert-danger'
+    elsif flash.notice
+      'alert-success'
+    end
+  end
+
+  def flash_message(flash)
+    if flash.alert
+      flash.alert
+    elsif flash.notice
+      flash.notice
+    end
+  end
 end
