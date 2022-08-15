fbb4f0cca89cba425e928d0614500263c674146d
llkadenll <kdwornik@o2.pl>
2022-08-15 20:33:31 +0200

    Add category vaildations

--- app/models/category.rb
+++ app/models/category.rb
@@ -1,3 +1,7 @@
 class Category < ApplicationRecord
   has_many :posts
+
+  validates :name, presence: true, uniqueness: { case_sensitive: false }
+  validates :icon, presence: true
+  validates :color, presence: true
 end
